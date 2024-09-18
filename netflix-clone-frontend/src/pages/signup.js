0168ecf02c986871988495446e61dd9c8141b26c
akshitmittal20 <akshitmittal20@gmail.com>
2024-09-18 15:33:08 +0530

    navbar fixing

--- netflix-clone-frontend/src/pages/signup.js
+++ netflix-clone-frontend/src/pages/signup.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import NavBar from '../../src/components/Navbar'
-
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
